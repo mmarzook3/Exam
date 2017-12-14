494207f6f49976295ed9887248ebc6f4af3acf41
Mohamed Mohamed Marzook <moham746@coventry.ac.uk>
2017-12-14 16:23:21 +0000

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double sum = 0, avarage, input;
+	int count = 0;
+	do
+	{
+		cout << "Enter " << count + 1 << " floating point number | press Ctrl + Z to end inputting " <<endl;
+		cin >> input;
+		sum += input;
+		count++;
+
+	} while (count < 20 && !cin.fail());
+
+	if (cin.fail())
+	{
+		sum -= input;
+	}
+	
+
+	avarage = sum / count;
+
+	cout << " Sum is : " << sum << " | Avarage is : " << avarage << endl;
+
+	system("PAUSE");
+	return 0;
+}
